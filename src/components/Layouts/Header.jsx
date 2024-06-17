afdd9ae846929eef40ada64cfc7526b53ddfa8ce
PeaceNaza <peacedonatus38@gmail.com>
2024-06-17 23:35:27 +0100

    chore: Update Header component styles for responsiveness

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/logo.svg";
 
 const Header = () => {
   return (
-    <div className="my-5 min-h-full bg-white">
+    <div className="py-5 min-h-full w-full bg-white">
       <Link to={"/"} className="flex gap-3 w-20">
         <img src={logo} alt="logo" className="w-10 h-10" />
         <p className="text-[#a855f7] text-2xl">MovieMate</p>
