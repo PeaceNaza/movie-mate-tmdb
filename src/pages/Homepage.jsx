d8bf51def7fa8444fcee7f3e36a6acf238e754a7
PeaceNaza <peacedonatus38@gmail.com>
2024-06-18 00:19:39 +0100

    Update poster and homepage layouts for better responsiveness

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
   return (
     <>
       <Header />
-      <main className="md:mt-10 xs:-mt-24 gap-5 flex md:flex-row xs:flex-col h-screen bg-white">
+      <main className="md:mt-10 xs:-mt-24 gap-5 flex md:flex-row xs:flex-col h-screen bg-white min-h-screen">
         <div className="w-1/2">
           <h1 className="text-3xl md:text-4xl lg:text-7xl mt-36 text-black">
             Learn more about your favourite movies
@@ -30,8 +30,8 @@ const Homepage = () => {
             <div className="col-start-1 xs:col-span-3">
               <Poster movieId={183} />
             </div>
-            <div className="col-start-1 xs:col-end-3"> </div>
-            <div className="col-end-7 col-span-3">
+            <div className="col-start-1 col-end-3"> </div>
+            <div className="col-end-7 col-span-3 xs:col-end-8 mb-20">
               <Poster movieId={20} />
             </div>
           </div>
