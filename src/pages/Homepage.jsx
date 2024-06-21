68b34ee724db43c5afbc42b91ef2600dfa88fecb
PeaceNaza <peacedonatus38@gmail.com>
2024-06-21 23:16:14 +0100

    style: Adjust movie poster width and grid layout in Poster and Homepage components

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,7 +4,7 @@ import Poster from "../components/Layouts/Poster";
 import Button from "../components/Button";
 import { Box, Flex, Group, Grid, RemoveScroll, Title } from "@mantine/core";
 import { useMediaQuery } from "@react-hook/media-query";
-import { persist } from 'zustand/middleware';
+import { persist } from "zustand/middleware";
 
 const Homepage = () => {
   const isDesktop = useMediaQuery("(min-width: 1024px)");
@@ -12,7 +12,9 @@ const Homepage = () => {
   const Content = (
     <Group gap="xl">
       <Box className="sm:w-1/2">
-        <Title order={1} fw={600} className="text-2xl lg:text-7xl md:text-4xl">Learn more about your favourite movies</Title>
+        <Title order={1} fw={600} className="text-3xl lg:text-7xl md:text-4xl">
+          Learn more about your favourite movies
+        </Title>
 
         <Flex gap="xl" mt={40}>
           <Link to={"/movies"}>
@@ -20,27 +22,23 @@ const Homepage = () => {
           </Link>
 
           <Link to={"/movies"}>
-            <Button variant="outline">
-              I am feeling lucky
-            </Button>
+            <Button variant="outline">I am feeling lucky</Button>
           </Link>
         </Flex>
       </Box>
- 
-      
+
       <Grid>
         <Grid.Col span={7}>
           <Grid gutter="5">
-        <Grid.Col span={9} offset={{base: 0, lg: 3}}>
-          <Poster movieId={183} />
-        </Grid.Col>
-        <Grid.Col span={9} offset={{base: 10.5, sm: 12}}>
-          <Poster movieId={20} />
-        </Grid.Col>
-        </Grid>
+            <Grid.Col span={9} offset={{ base: 0, lg: 3 }}>
+              <Poster movieId={183} />
+            </Grid.Col>
+            <Grid.Col span={9} offset={{ base: 10.5, lg: 12, sm: 9 }}>
+              <Poster movieId={20} />
+            </Grid.Col>
+          </Grid>
         </Grid.Col>
       </Grid>
-    
     </Group>
   );
 
